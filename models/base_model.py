a64b44a3ff75aedb6e399cc1f1e7e5aa901fe0d1
JoelInyang <joelinyang2003@gmail.com>
2022-11-23 21:42:08 -0800

    f

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,23 @@ class BaseModel:
     A class BaseModel that defines all 
     commomn attributes for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         The constructor function
         """
+        tformat = %Y-%m-%dT%H:%M:%S.%f
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or "updated_at":
+                    self.__dict__[key] = datetime.strptime(v, tformat)
+                else:
+                    self.__dict__[key] = value
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """
