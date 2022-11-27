0d01d3f4ef2eaf27bd8e8482cc808134f75d1c74
JoelInyang <joelinyang2003@gmail.com>
2022-11-27 12:31:17 -0800

    added files

--- models/base_model.py
+++ models/base_model.py
@@ -1,59 +1,68 @@
 #!/usr/bin/python3
-"""Module for Base class
-Contains the Base class for the AirBnB clone console.
+# -*- coding: utf-8 -*-
 """
-
+Creation date: June 26, 2020
+Authors: Robinson Montes
+        Carlos Murcia
+"""
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
-
-    """Class for base model of object hierarchy."""
-
+    """
+    base_model that defines all common attributes/methods for other classes
+    """
     def __init__(self, *args, **kwargs):
-        """Initialization of a Base instance.
-        Args:
-            - *args: list of arguments
-            - **kwargs: dict of key-values arguments
+        """init method for BaseModel Class
+        Attributes:
+            args (list): inputted arguments as a list.
+            kwargs (dict): inputted arguments as a dict.
+            id (str) - assign with an uuid when an instance is created.
+            created_at (time): datetime - assign with the current datetime when
+                an instance is created.
+            updated_at (time): datetime - assign with the current datetime when
+                n instance is created and it will be updated every time you
+                change your object.
         """
-
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
+        if len(kwargs) > 0:
+            for k, v in kwargs.items():
+                if k in ['created_at', 'updated_at']:
+                    self.__dict__[k] = datetime\
+                                       .strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == 'id':
+                    self.id = v
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
-        """Returns a human-readable string representation
-        of an instance."""
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+        """str method for BaseModel Class
+            Return:
+                string (str): string descriptor for BaseModel Class
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
 
     def save(self):
-        """Updates the updated_at attribute
-        with the current datetime."""
-
+        """
+        updates the public instance attribute updated_at with the
+        current datetime
+        """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary representation of an instance."""
-
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+        """returns a dictionary containing all keys/values of __dict__
+        of the instance
+        Return:
+            dictionary (dict): Dictionary object that contains __dict__
+        """
+        dictionary = self.__dict__.copy()
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary["__class__"] = self.__class__.__name__
+        return dictionary
